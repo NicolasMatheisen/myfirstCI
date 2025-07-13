831d27650ff294757162d6df8e5ba94ed5b67e4f
Nicolas Matheisen <nicolasmatheisen@icloud.com>
2025-07-13 05:27:57 +0200

    chore: remove Prettier configuration and related scripts from project

--- index.js
+++ index.js
@@ -3,23 +3,22 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 rl.question('Gib die erste Zahl ein: ', (input1) => {
-	rl.question('Gib die zweite Zahl ein: ', (input2) => {
-		const zahl1 = parseFloat(input1);
-		const zahl2 = parseFloat(input2);
+  rl.question('Gib die zweite Zahl ein: ', (input2) => {
+    const zahl1 = parseFloat(input1);
+    const zahl2 = parseFloat(input2);
 
-		if (isNaN(zahl1) || isNaN(zahl2)) {
-			console.log('Bitte gültige Zahlen eingeben!');
-		}
-		else {
-			const summe = zahl1 + zahl2;
-			console.log(`Die Summe ist: ${summe}`);
-		}
+    if (isNaN(zahl1) || isNaN(zahl2)) {
+      console.log('Bitte gültige Zahlen eingeben!');
+    } else {
+      const summe = zahl1 + zahl2;
+      console.log(`Die Summe ist: ${summe}`);
+    }
 
-		rl.close();
-	});
+    rl.close();
+  });
 });
